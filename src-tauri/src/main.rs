b54ccd9948bb6cd208b5fdf8d95e97bb39e94837
Manu <emmanuel.botros@heyteam.com>
2025-02-07 21:14:10 +0400

    fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,10 @@
 // Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let window = app.get_window("main").unwrap();
+            let window = app.get_webview_window("main").unwrap();
             
             #[cfg(debug_assertions)]
             window.open_devtools();
